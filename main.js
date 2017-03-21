250be7a407f0d02be02e995ccd617101cd37ee15
Jennifer Meier <meierjl01@gmail.com>
2017-03-21 13:38:50 -0500

    initial files

---
+++ main.js
@@ -0,0 +1,5 @@
+//1. FizzBuzz
+//Print the numbers 1..100
+//For multiples of 3, print "Fizz" instead of the number
+//For multiples of 5, print "Buzz" instead of the number
+//For multiples of 3 and 5, print "FizzBuzz" instead of the number
